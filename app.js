a06dbb7e6c3195d6d2b1f7cf53a8bf4aab637808
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-03-26 18:17:31 -0400

    define API Routes
    - GET tasks
    - POST new tasks
    - PUT edit tasks
    - DELETE tasks

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Initialize Express app
 const app = express();
 const port = process.env.PORT|| 3000;
 
-//Middleware
+// Middleware setup
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,7 +13,47 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // task
 const tasks = [];
 
-//API routes
+// API routes
+// GET route for retrieving all tasks
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
+// POST route for creating a new task
+app.post('/tasks', (req, res) => {
+    const task = req.body;
+    tasks.push(task);
+    res.status(201).json(task);
+});
+
+// PUT route for updating an existing task
+app.put('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const updatedTask = req.body;
+
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+    if(taskIndex !== -1){
+        tasks[taskIndex] = updatedTask;
+        res.json(updatedTask);
+    }else{
+        res.status(404).json({ message: `Task with id ${taskId} not found` });
+    }
+});
+
+// DELETE route for deleting a task
+app.delete('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+    if(taskIndex !== -1){
+        tasks.splice(taskIndex, 1);
+        res.status(204).send();
+    }else{
+        res.status(404).json({ message: `Task with id ${taskId} not found` });
+    }
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log('server is running on port ${port}');
+    console.log(`server is running on port ${port}`);
 });
